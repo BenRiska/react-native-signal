782f07ef812b61ac259015b8a8709c8e94bcb0fe
bentaylor <ben10taylor10@hotmail.com>
2021-04-04 19:00:01 +0100

    initial commit

---
+++ firebase.js
@@ -0,0 +1,10 @@
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDM23LZrVtG5xFS0Wbbtiu-qTuDNYNc3Cs",
+    authDomain: "react-native-c87dd.firebaseapp.com",
+    projectId: "react-native-c87dd",
+    storageBucket: "react-native-c87dd.appspot.com",
+    messagingSenderId: "989345905653",
+    appId: "1:989345905653:web:c22446212d0fa8358d3ebc"
+  };
